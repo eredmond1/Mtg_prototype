5fc5e5db6d170a30390d57a3ef931e6cf1238a6e
eredmond1 <150072405+eredmond1@users.noreply.github.com>
2025-08-16 17:35:27 -0600

    Main phase implementation

--- Mtg_game/GameStateManagers/TurnState/main_phase.py
+++ Mtg_game/GameStateManagers/TurnState/main_phase.py
@@ -2,4 +2,30 @@
 class MainPhase:
     
     def execute(self, player):
-        return 
+        valid_action = False
+        mp_action = input("play a card: 1 \nend main phase: 2\n ")
+        while valid_action == False:
+            
+            if mp_action == "1":
+                valid_action = True
+                self.play_card(player)
+            elif mp_action =="2":
+                valid_action = True
+            else:
+                print("Please select a valid option\n")
+                
+                
+    def play_card(self, player):
+        player.print_hand()
+        card_choice = int(input("select the card that you want to play: "))
+        if ( card_choice <= player.get_hand_count()):
+            valid_action = player.play_card(card_choice)
+            if valid_action: return 
+            
+        else:
+            print("you can't play this card")
+            return
+                
+            
+        
+        
